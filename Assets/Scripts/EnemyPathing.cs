a4d0b7b9d69d1076d7de266c399bd5d8a6acd456
AaSleigher <quaper1@gmail.com>
2020-09-01 23:10:47 -0500

    Made wave movements work

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
 
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> waypoints;
     [SerializeField] float moveSpeed = 2f;
     int waypointIndex = 0;
 
     private void Start()
     {
+        waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].transform.position;
     }
 
--- Assets/Scripts/WaveConfig.cs
+++ Assets/Scripts/WaveConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 
@@ -7,19 +8,32 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Enemy Wave Config")]
 public class WaveConfig : ScriptableObject
 {
-
+    
     [SerializeField] GameObject enemyPrefab;
     [SerializeField] GameObject pathPrefab;
-    [SerializeField] float timeBetweenSpawns = 0.5f;
     [SerializeField] float spawnRandomFactor = 0.3f;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
     [SerializeField] int numberOfEnemies = 5;
     [SerializeField] float moveSpeed = 2f;
 
     public GameObject GetEnemyPrefab() { return enemyPrefab; }
-    public GameObject GetPathPrefab() { return pathPrefab; }
+
+    public List<Transform> GetWaypoints() 
+    {
+        var waveWaypoints = new List<Transform>();
+        foreach (Transform child in pathPrefab.transform)
+        {
+            waveWaypoints.Add(child);
+        }
+        return waveWaypoints; 
+    }
+
     public float GetTimeBetweenSpawns() { return timeBetweenSpawns; }
+
     public float GetSpawnRandomFactor() { return spawnRandomFactor; }
+
     public int GetNumberOfEnemies() { return numberOfEnemies; }
+
     public float GetMoveSpeed() { return moveSpeed; }
 
 
